4fcf747068b1d9b81b13ec4a1f66b0c8fa68c942
sheetalsingh77 <sheetalchrnl@gmail.com>
2021-03-24 01:31:54 +0530

    splash

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import HomeNavigator from './src/HomeNavigator'
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  },[])
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
